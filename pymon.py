9fd8d88ef0a8e9b634b16f5a0dd605da9cce7dd3
Nick <55280957+NAEK2K@users.noreply.github.com>
2022-03-29 00:13:37 -0400

    Create pymon.py

---
+++ pymon.py
@@ -0,0 +1,43 @@
+import argparse
+import hashlib
+import time
+from threading import Thread
+import subprocess
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--f")
+    parser.add_argument("-a", "--a")
+    return parser.parse_args()
+
+
+def get_hash_of_file(f):
+    return hashlib.md5(open(f, "r").read().encode()).hexdigest()
+
+
+def compare_hash(h1, h2):
+    return h1 == h2
+
+
+def run_program(f, a):
+    return subprocess.Popen((["py", f, a if a else ""]))
+
+
+def get_program_process(f, a):
+    return Thread(target=run_program, args=[f, a])
+
+
+if __name__ == "__main__":
+    args = get_args()
+    program = run_program(args.f, args.a)
+    previous_hash = get_hash_of_file(args.f)
+    while True:
+        new_hash = get_hash_of_file(args.f)
+        if previous_hash != new_hash:
+            print("Change detected in file, restarting.")
+            program.terminate()
+            program = run_program(args.f, args.a)
+            previous_hash = new_hash
+        time.sleep(1)
+        
